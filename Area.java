e3df4c85190a1187f169f79b10df0695048d484c
Chiranthcv <chiranth775@gmail.com>
2025-03-18 12:25:12 +0530

    testone

--- Area.java
+++ Area.java
@@ -1,12 +1,13 @@
-java.util.scanner;
+java.util.Scanner;
 public class Area{
 	public static void main(String[]args){
 		Scanner sc = new Scanner(System.in);
 		double height = scanner.nextDouble();
 		double base = scanner.nextDouble();
 		double area = 0.5*height*base;
-		System.out.println("Area of the triangle");
+		System.out.println("Area of the triangle"+area);
 		Scanner.close();
 	}
 }
+
 
